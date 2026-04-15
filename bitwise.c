2653b918a660892fba3872ef4e0f4c22289ca469
sathiyabamajaganathan5bci27-cell <sathiyabamajaganathan5_bci27@mepcoeng.ac.in>
2026-04-15 12:00:06 +0530

    Create bitwise.c

---
+++ bitwise.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void calculate_the_maximum(int n, int k) {
+    int max_and = 0, max_or = 0, max_xor = 0;
+
+    // Nested loops to find all pairs (i, j) where 1 <= i < j <= n
+    for (int i = 1; i <= n; i++) {
+        for (int j = i + 1; j <= n; j++) {
+            int current_and = i & j;
+            int current_or = i | j;
+            int current_xor = i ^ j;
+
+            // Check AND
+            if (current_and < k && current_and > max_and) {
+                max_and = current_and;
+            }
+            // Check OR
+            if (current_or < k && current_or > max_or) {
+                max_or = current_or;
+            }
+            // Check XOR
+            if (current_xor < k && current_xor > max_xor) {
+                max_xor = current_xor;
+            }
+        }
+    }
+
+    printf("%d\n%d\n%d\n", max_and, max_or, max_xor);
+}
+
+int main() {
+    int n, k;
+    scanf("%d %d", &n, &k);
+    calculate_the_maximum(n, k);
+    return 0;
+}
