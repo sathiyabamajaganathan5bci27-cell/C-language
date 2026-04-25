cfccb5713742951a5bf173c395bab89cb99ba059
sathiyabamajaganathan5bci27-cell <sathiyabamajaganathan5_bci27@mepcoeng.ac.in>
2026-04-25 11:38:25 +0530

    Create matrix addition.c

---
+++ matrix addition.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int a[2][2] = {{1,2},{3,4}}, b[2][2] = {{5,6},{7,8}}, sum[2][2];
+    for(int i=0; i<2; i++)
+        for(int j=0; j<2; j++) {
+            sum[i][j] = a[i][j] + b[i][j];
+            printf("%d ", sum[i][j]);
+        }
+    return 0;
+}
